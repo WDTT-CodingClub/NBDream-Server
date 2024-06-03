3ed8d7d837cbe10482be612c1cd3d2a39b2dd21d
이정우 <acg6138@naver.com>
2024-06-03 09:44:04 +0900

    프로젝트 생성 및 라이브러리 세팅

---
+++ src/main/java/nbdream/NbdreamApplication.java
@@ -0,0 +1,13 @@
+package nbdream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbdreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbdreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nbdream/NbdreamApplicationTests.java
@@ -0,0 +1,13 @@
+package nbdream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbdreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
