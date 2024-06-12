e223112a8417b1ec262a92c264742b05ba8427b1
이정우 <acg6138@naver.com>
2024-06-12 16:38:19 +0900

    test

--- src/main/java/nbdream/image/controller/ImageController.java
+++ src/main/java/nbdream/image/controller/ImageController.java
@@ -1,18 +1,16 @@
 package nbdream.image.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import nbdream.common.advice.response.ApiResponse;
 import nbdream.common.exception.NotFoundException;
 import nbdream.image.service.ImageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -21,23 +19,25 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "Image Controller")
 public class ImageController {
 
     private final ImageService imageService;
 
-    @PostMapping("/{domain}/images")
-    public ResponseEntity<String> uploadImage(@PathVariable("domain") String domain, MultipartFile image, HttpServletRequest request) {
+    @Operation(summary = "이미지 업로드")
+    @PostMapping(value = "/{domain}/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<String> uploadImage(@PathVariable("domain") String domain, MultipartFile image, @RequestPart HttpServletRequest request) {
         if (domain == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid domain");
+            return ApiResponse.ok();
         }
-
         try {
-            return ResponseEntity.ok(imageService.saveImage(image, domain));
+            return ApiResponse.ok(imageService.saveImage(image, domain));
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Image upload failed");
+            return ApiResponse.ok();
         }
     }
 
+    @Operation(summary = "이미지 가져오기")
     @GetMapping("/{domain}/{id}")
     public ResponseEntity<byte[]> getImages(@PathVariable String domain, @PathVariable Long targetId) {
         try {
@@ -48,6 +48,7 @@ public class ImageController {
         }
     }
 
+    @Operation(summary = "이미지 삭제")
     @DeleteMapping("/{domain}/{filename}")
     public ResponseEntity<String> deleteImage(@PathVariable String domain, @PathVariable(name = "filename") String fileName) {
         try {
