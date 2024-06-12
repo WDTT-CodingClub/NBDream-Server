c48f104e865b746593ff87ee36b793140d9aca74
practise30 <practise30@gmail.com>
2024-06-13 08:53:18 +0900

    Update BulletinService.java

--- src/main/java/nbdream/bulletin/service/BulletinService.java
+++ src/main/java/nbdream/bulletin/service/BulletinService.java
@@ -1,4 +1,9 @@
 package nbdream.bulletin.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BulletinService {
 }
