21c4527cfba030df3033897ccccc1acf59d5ccf2
ekzot1212 <ekzot1212@gmail.com>
2024-06-14 17:19:13 +0900

    Refactor : Swagger 어노테이션 추가

--- src/main/java/nbdream/accountBook/controller/AccountBookController.java
+++ src/main/java/nbdream/accountBook/controller/AccountBookController.java
@@ -1,5 +1,7 @@
 package nbdream.accountBook.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import nbdream.accountBook.service.AccountBookHistoryService;
 import nbdream.accountBook.service.AccountBookService;
@@ -12,12 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth/account")
+@Tag(name = "AccountBook Controller")
 public class AccountBookController {
 
     private final AccountBookService accountBookService;
     private final AccountBookHistoryService accountBookHistoryService;
 
-    //내 장부 조회
+    @Operation(summary = "내 장부 조회", description = "")
     @GetMapping
     public ApiResponse<GetAccountBookListResDto> getMyAccountBookList( @RequestBody GetAccountBookListReqDto request,
                                                                        @AuthenticatedMemberId Long memberId){
@@ -25,14 +28,14 @@ public class AccountBookController {
         return ApiResponse.ok(response);
     }
 
-    //장부 작성
+    @Operation(summary = "장부 작성", description = "")
     @PostMapping("/register")
     public ApiResponse<Void> writeAccountBookHistory(@RequestBody PostAccountBookReqDto request,
                                                      @AuthenticatedMemberId Long memberId) {
         return accountBookHistoryService.writeAccountBookHistory(request, memberId);
     }
 
-    //장부 내역 수정
+    @Operation(summary = "장부 내역 수정", description = "")
     @PutMapping("/update/{account-book-history-id}")
     public ApiResponse<Void> updatePost(@RequestBody PutAccountBookReqDto request,
                                         @AuthenticatedMemberId Long memberId,
@@ -40,14 +43,15 @@ public class AccountBookController {
         return accountBookHistoryService.updateAccountBookHistory(request, memberId, accountBookHistoryId);
     }
 
-    //장부 상세 조회
+    @Operation(summary = "장부 상세 조회", description = "")
     @GetMapping("/detail/{account-book-history-id}")
     public ApiResponse<GetAccountBookDetailResDto> getMyAccountBookList(@AuthenticatedMemberId Long memberId,
                                                                         @PathVariable("account-book-history-id") final Long accountBookHistoryId){
         GetAccountBookDetailResDto response = accountBookHistoryService.getAccountBookDetail(memberId, accountBookHistoryId);
         return ApiResponse.ok(response);
     }
-    //장부 삭제
+
+    @Operation(summary = "장부 삭제", description = "")
     @DeleteMapping("/delete/{account-book-history-id}")
     public ApiResponse<Void> deleteAccountBookHistory(@AuthenticatedMemberId Long memberId,
                                                       @PathVariable("account-book-history-id") final Long accountBookHistoryId) {
