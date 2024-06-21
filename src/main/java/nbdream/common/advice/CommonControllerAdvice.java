799764b998cd980f3a2eb7a8298fe9b8f154aa0e
이정우 <acg6138@naver.com>
2024-06-21 12:29:17 +0900

    Feat: 에러 발생 시 HTTP 상태가 반환되도록 수정

--- src/main/java/nbdream/common/advice/CommonControllerAdvice.java
+++ src/main/java/nbdream/common/advice/CommonControllerAdvice.java
@@ -7,11 +7,14 @@ import nbdream.common.exception.BadRequestException;
 import nbdream.common.exception.InvalidFormatException;
 import nbdream.common.exception.NotFoundException;
 import nbdream.common.exception.UnauthorizedException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.io.IOException;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -24,32 +27,32 @@ public class CommonControllerAdvice {
             HttpMessageNotReadableException.class,
             MethodArgumentNotValidException.class
     })
-    public ApiResponse<Void> handleBadRequest() {
-        return ApiResponse.of(BAD_REQUEST, "입력 형식 또는 타입이 올바르지 않습니다.");
+    public ResponseEntity<ApiResponse> handleBadRequest() {
+        return ResponseEntity.badRequest().body(ApiResponse.of(BAD_REQUEST, "입력 형식 또는 타입이 올바르지 않습니다."));
     }
 
     @ExceptionHandler({BadRequestException.class, InvalidFormatException.class})
-    public ApiResponse<Void> handleBadRequest(final Exception e) {
+    public ResponseEntity<ApiResponse> handleBadRequest(final Exception e) {
         log.debug("HandleBadRequest : {}", e.getMessage());
-        return ApiResponse.of(BAD_REQUEST, e.getMessage());
+        return ResponseEntity.badRequest().body(ApiResponse.of(BAD_REQUEST, e.getMessage()));
     }
 
     @ExceptionHandler({UnauthorizedException.class, JwtException.class})
-    public ApiResponse<Void> handleUnauthorized(final Exception e) {
+    public ResponseEntity<ApiResponse> handleUnauthorized(final Exception e) {
         log.debug("UnauthorizedException : {}", e.getMessage());
         log.debug("Exception Stack Trace : \n{}", e.getStackTrace());
-        return ApiResponse.of(UNAUTHORIZED, e.getMessage());
+        return ResponseEntity.status(UNAUTHORIZED).body(ApiResponse.of(UNAUTHORIZED, e.getMessage()));
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ApiResponse<Void> handleNotFound(final Exception e) {
+    public ResponseEntity<ApiResponse> handleNotFound(final Exception e) {
         log.debug("NotFoundException : {}", e.getMessage());
-        return ApiResponse.of(NOT_FOUND, e.getMessage());
+        return ResponseEntity.status(NOT_FOUND).body(ApiResponse.of(NOT_FOUND, e.getMessage()));
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ApiResponse<Void> handleInternalServerError(RuntimeException e) {
+    @ExceptionHandler({RuntimeException.class, IOException.class})
+    public ResponseEntity<ApiResponse> handleInternalServerError(RuntimeException e) {
         log.debug("RuntimeException : {}", e.getMessage());
-        return ApiResponse.of(INTERNAL_SERVER_ERROR, e.getMessage());
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(ApiResponse.of(INTERNAL_SERVER_ERROR, e.getMessage()));
     }
 }
