4f8d8877c0c17a61e17881e6955363612857cf4c
practise30 <practise30@gmail.com>
2024-06-25 23:44:08 +0900

    Update FarmingDiaryController.java

--- src/main/java/nbdream/farm/controller/FarmingDiaryController.java
+++ src/main/java/nbdream/farm/controller/FarmingDiaryController.java
@@ -1,5 +1,7 @@
 package nbdream.farm.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import nbdream.common.advice.response.ApiResponse;
 import nbdream.farm.service.FarmingDiaryFacade;
@@ -18,35 +20,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/calendar/diary")
+@Tag(name = "FarmingDiary Controller")
 public class FarmingDiaryController {
 
     private final FarmingDiaryFacade farmingDiaryFacade;
-    private final FarmingDiaryService farmingDiaryService;
+
 
     @PostMapping
+    @Operation(summary = "영농일지 작성")
     public ApiResponse<Long> createFarmingDiary(@RequestBody FarmingDiaryRequest request){
         Long farmingDiaryId = farmingDiaryFacade.saveFarmingDiaryInfo(request);
         return ApiResponse.ok(farmingDiaryId);
     }
 
     @PutMapping("/{diary-id}")
+    @Operation(summary = "영농일지 수정")
     public ApiResponse<Long> updateFarmingDiary(@PathVariable(name = "diary-id") Long farmingDiaryId,
                                    @RequestBody FarmingDiaryRequest request) {
         Long updatedFarmingDiaryId = farmingDiaryFacade.editFarmingDiaryInfo(farmingDiaryId, request);
         return ApiResponse.ok(updatedFarmingDiaryId);
     }
 
+
     @DeleteMapping("/{diary-id}")
+    @Operation(summary = "영농일지 삭제")
     public ApiResponse<Void> deleteFarmingDiary(@PathVariable(name = "diary-id") Long farmingDiaryId) {
         farmingDiaryFacade.deleteFarmingDiaryInfo(farmingDiaryId);
         return ApiResponse.ok();
     }
 
-    @GetMapping
-    public void getFarmingDiaries(){}
-
 
     @GetMapping("/{diary-id}")
+    @Operation(summary = "영농일지 상세 조회")
     public ApiResponse<FarmingDiaryResponse> getFarmingDiary(@PathVariable(name = "diary-id") Long farmingDiaryId) {
         FarmingDiaryResponse response = farmingDiaryFacade.fetchFarmingDiaryInfoDetail(farmingDiaryId);
         return ApiResponse.ok(response);
