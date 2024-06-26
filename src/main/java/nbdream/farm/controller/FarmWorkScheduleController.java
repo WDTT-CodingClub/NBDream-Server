c18c16af67a8861b6351cbcf112130e9f85d3243
ekzot1212 <ekzot1212@gmail.com>
2024-06-27 00:14:14 +0900

    Fix : 농작업 일정 조회 아예 정보가 없는 경우 빈 객체 반환
    농작업 일정 조회 시 데이터가 아예 없는 경우 ex) 고구마 12월
    기존   : 예외
    변경 후 : 빈 객체 반환

--- src/main/java/nbdream/farm/controller/FarmWorkScheduleController.java
+++ src/main/java/nbdream/farm/controller/FarmWorkScheduleController.java
@@ -6,12 +6,15 @@ import nbdream.farm.service.FarmWorkScheduleService;
 import org.springframework.web.bind.annotation.RestController;
 
 
+//@RequestMapping("/api/farm-work-open-api")
 @RestController
 @RequiredArgsConstructor
 public class FarmWorkScheduleController {
 
     private final FarmWorkScheduleService farmWorkScheduleService;
 
+//    @Operation(summary = "농작업 일정 Open Api 테스트", description = "")
+//    @GetMapping
     public ApiResponse<Void> saveWorkSchedule(){
         farmWorkScheduleService.saveWorkSchedule();
         return ApiResponse.ok();
--- src/main/java/nbdream/farm/service/ScheduleService.java
+++ src/main/java/nbdream/farm/service/ScheduleService.java
@@ -3,7 +3,6 @@ package nbdream.farm.service;
 import lombok.RequiredArgsConstructor;
 import nbdream.accountBook.exception.CategoryNotFoundException;
 import nbdream.common.advice.response.ApiResponse;
-import nbdream.common.exception.UnauthorizedException;
 import nbdream.farm.domain.Crop;
 import nbdream.farm.domain.Farm;
 import nbdream.farm.domain.FarmWorkSchedule;
@@ -12,15 +11,10 @@ import nbdream.farm.exception.*;
 import nbdream.farm.repository.*;
 import nbdream.farm.service.dto.schedule.request.*;
 import nbdream.farm.service.dto.schedule.response.FarmWorkListResDto;
-import nbdream.farm.service.dto.schedule.response.FarmWorkResDto;
 import nbdream.farm.service.dto.schedule.response.ScheduleListResDto;
 import nbdream.farm.service.dto.schedule.response.ScheduleResDto;
-import nbdream.member.domain.Member;
-import nbdream.member.exception.MemberNotFoundException;
-import nbdream.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,7 +22,6 @@ import java.util.List;
 public class ScheduleService {
 
     private final FarmWorkScheduleCustomRepository farmWorkScheduleCustomRepository;
-    private final MemberRepository memberRepository;
     private final FarmRepository farmRepository;
     private final ScheduleRepository scheduleRepository;
     private final CropRepository cropRepository;
@@ -41,11 +34,9 @@ public class ScheduleService {
             throw new CropFromWorkScheduleNotFoundException();
         }
 
-        List<FarmWorkSchedule> workScheduleList = farmWorkScheduleCustomRepository.findSchedulesByCropAndMonth(request.getCrop(), request.getMonth());
-        if(workScheduleList == null || workScheduleList.isEmpty()){
-            throw new FarmWorkScheduleNotFoundException();
-        }
+        validateMonth(request.getMonth());
 
+        List<FarmWorkSchedule> workScheduleList = farmWorkScheduleCustomRepository.findSchedulesByCropAndMonth(request.getCrop(), request.getMonth());
         return new FarmWorkListResDto().createFarmWorkListResDto(workScheduleList);
     }
 
@@ -121,6 +112,12 @@ public class ScheduleService {
             }
         }
         throw new CategoryNotFoundException();
+    }
+
+    private void validateMonth(int month) {
+        if(month < 1 || month > 12) {
+            throw new InvalidDateFormatException();
+        }
     }
 
 }
