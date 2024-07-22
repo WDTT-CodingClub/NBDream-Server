b215c6231a2442071963664822e13805dabc438e
이정우 <acg6138@naver.com>
2024-07-22 22:19:41 +0900

    Fix: dto 수정

--- src/main/java/nbdream/comment/dto/CommentResDto.java
+++ src/main/java/nbdream/comment/dto/CommentResDto.java
@@ -1,5 +1,6 @@
 package nbdream.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,7 +28,9 @@ public class CommentResDto {
     private String profileImageUrl;
     private String content;
     private Boolean isAuthor;
+    @JsonFormat(timezone = "Asia/seoul")
     private LocalDateTime createdDate;
+    @JsonFormat(timezone = "Asia/seoul")
     private LocalDateTime lastModifiedDate;
 
 
