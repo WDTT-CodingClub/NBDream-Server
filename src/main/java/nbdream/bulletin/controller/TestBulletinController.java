655d4951e6dba49c8a4bd13755c8ff5aa6fe03e1
practise30 <practise30@gmail.com>
2024-08-21 19:08:45 +0900

    Update TestBulletinController.java

--- src/main/java/nbdream/bulletin/controller/TestBulletinController.java
+++ src/main/java/nbdream/bulletin/controller/TestBulletinController.java
@@ -5,6 +5,7 @@ import nbdream.bulletin.dto.request.BulletinTestDto;
 import nbdream.bulletin.service.TestBulletinService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,7 +19,7 @@ public class TestBulletinController {
     private final TestBulletinService service;
 
     @PostMapping("/test")
-    public void test(@RequestPart(name = "dto", required = false)List<MultipartFile> files) {
+    public void test(@RequestPart(name = "contents",required = false)String content, @RequestPart(name = "dto", required = false)List<MultipartFile> files) {
         service.uploadTestBulletin(files);
     }
 }
